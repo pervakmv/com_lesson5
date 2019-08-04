8b739daf0d305a3081a5dbe2b4bfd1cc90d2b059
Nikolay <pervakmv@elas.kiev.ua>
2019-08-04 18:21:30 +0300

    It's work

--- ItemDAO.java
+++ ItemDAO.java
@@ -1,54 +1,48 @@
 package com.lesson5;
 
 import com.lesson5.Exception.InternalServerErrorException;
-import org.hibernate.HibernateError;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.hibernate.query.NativeQuery;
 import org.springframework.stereotype.Repository;
-
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
-
 @Repository
 @Transactional
-public class ItemDAO extends CustomDAO<Item> {
-
+public class ItemDAO {
 
     @PersistenceContext
     private EntityManager entityManager;
 
-    @Transactional
+
     public Item save(Item item) {
-        if (entityManager != null)
-            entityManager.persist(item);
-        else
-            System.out.println("entity manager is null");
-        //System.out.println(item);
+        entityManager.persist(item);
         return item;
     }
 
-    public List<Item> listItem() throws InternalServerErrorException {
-        List<Item> resList = new ArrayList();
-        try (SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-             Session session = sessionFactory.openSession()) {
-            session.beginTransaction();
-            Query query = session.createQuery("from Item");
-            resList = query.list();
-            session.getTransaction().commit();
-        } catch (HibernateError e) {
-            throw new InternalServerErrorException("get list of Item with error");
-        }
-        return resList;
+
+    public Item delete(Long id) {
+        Item item = entityManager.find(Item.class, id);
+        entityManager.remove(item);
+        return item;
     }
 
+    public Item update(Item item) {
+        item = entityManager.merge(item);
+        return item;
+    }
+
+
+    public Item findById(long id) {
+        return entityManager.find(Item.class, id);
+    }
+
+
+    public List listItem() {
+        return entityManager.createQuery("from Item", Item.class).getResultList();
+    }
 }
